0dd914d0f511adb442219ca4b071a25dcc01875b
Ayan Dave <daveayan@gmail.com>
2017-01-30 00:04:14 -0500

    Fixed a few e2e tests

--- e2e/student-dashboard.e2e-spec.ts
+++ e2e/student-dashboard.e2e-spec.ts
@@ -14,19 +14,19 @@ describe('Student Dashboard Page', function() {
     newStudentPO.validateIntegrityOfPage();
   });
 
-  it('has a list of teachers in database', () => {
+  it('has a list of classes in database', () => {
     listStudentPO.listShouldHave('Magneta, Grade 1');
     listStudentPO.listShouldHave('Tornado, Grade K');
   });
 
-  it('can add a new teacher to the list', () => {
+  it('can add a new class to the list', () => {
     newStudentPO.addStudent('Hurricane', '2');
     listStudentPO.listShouldHave('Magneta, Grade 1');
     listStudentPO.listShouldHave('Tornado, Grade K');
     listStudentPO.listShouldHave('Hurricane, Grade 2');
   });
 
-  it('remove teacher from list', () => {
+  it('remove class from list', () => {
     listStudentPO.removeStudent('Hurricane, Grade 2');
     listStudentPO.listShouldHave('Magneta, Grade 1');
     listStudentPO.listShouldHave('Tornado, Grade K');
